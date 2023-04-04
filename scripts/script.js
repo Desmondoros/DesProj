5b719d9b3159a98b7ef46cb26dd0645b20261b43
Desmondoros <qwertyuiop7309@gmail.com>
2023-04-04 16:10:50 +0100

    Check out this awesome dicesgit status! ☺

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,14 @@
-console.log("Hello world");
+let min = 1;
+let max = 7;
+
+function getRandomArbitrary(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+let result1 = getRandomArbitrary(min, max);
+
+document.getElementById("int1").innerHTML=result1;
+
+let result2 = getRandomArbitrary(min, max);
+
+document.getElementById("int2").innerHTML=result2;
